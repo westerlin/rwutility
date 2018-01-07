c235f5ac8184e4cf8bb2d274bebde69b11717cad
Rasmus Westerlin <rasmuswesterlin@gmail.com>
2018-01-07 22:36:33 +0100

    First commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='rwutility',
-   version='1.0',
+   version='2.0',
    description='Simple little package for control of terminal input and output',
    author='Rasmus Westerlin',
    author_email='rasmuswesterlin@gmail.com',
